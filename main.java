88c80be7c7bcc53237e8e2fedded825f5318d48a
smbrown22 <smbrown22@my.waketech.edu>
2026-04-29 12:53:17 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,40 @@
+import java.net.URL;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a city name: ");
+        String city = input.nextLine();
+
+        String apiKey = "39c4692421824b45885131102262304";
+        
+        String urlString = "http://api.weatherapi.com/v1/forecast.json?key=" + apiKey +"&q=" + city + "&days=1&aqi=no&alerts=no";
+
+        try {
+            
+
+            URL url = new URL(urlString); 
+            Scanner urlScanner = new Scanner(url.openStream());
+
+            StringBuilder jsonData = new StringBuilder();
+            
+            while (urlScanner.hasNext()) {
+                jsonData.append(urlScanner.nextLine());
+            }
+            
+            urlScanner.close();
+
+            System.out.println("Weather Data: ");
+
+            System.out.println(jsonData.toString());
+
+        } catch (Exception ex) {
+
+            System.out.println("Error: Could not retrieve data. " + ex.getMessage());
+
+        }
+        
+        input.close();
+    }
+}
