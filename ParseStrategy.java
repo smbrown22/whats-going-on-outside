d6a520a974e91fcb4c892491952e543d09f2ced5
smbrown22 <smbrown22@my.waketech.edu>
2026-05-01 22:39:57 -0400

    Delete ParseStrategy.java

--- ParseStrategy.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface ParseStrategy {
-    // this just tells the app we need a way to pull data out of the text
-    String extractData(String jsonResponse) throws Exception;
-}
