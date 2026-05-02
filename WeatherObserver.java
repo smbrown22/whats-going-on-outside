7120cff067221740610c16aa2e3be759560bdffd
smbrown22 <smbrown22@my.waketech.edu>
2026-05-01 22:39:17 -0400

    Delete WeatherObserver.java

--- WeatherObserver.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface WeatherObserver {
-    // this lets the model send new weather data to the ui
-    void updateWeather(String weatherData);
-
-    // this sends a heads up if something goes wrong
-    void updateError(String errorMessage);
-}
